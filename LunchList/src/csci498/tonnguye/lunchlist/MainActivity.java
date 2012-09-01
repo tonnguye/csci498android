cc81440575b3cddb2f0b31c6740168ee3d8ff64a
Tony Nguyen <tonnguye@mymail.mines.edu>
2012-08-31 18:02:08 -0600

    Added radiobutton functionality

--- LunchList/src/csci498/tonnguye/lunchlist/MainActivity.java
+++ LunchList/src/csci498/tonnguye/lunchlist/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RadioGroup;
 
 public class MainActivity extends Activity {
 	
@@ -25,6 +26,19 @@ public class MainActivity extends Activity {
 
 			r.setName(name.getText().toString());
 			r.setAddress(address.getText().toString());
+			
+			RadioGroup types=(RadioGroup)findViewById(R.id.types);
+			switch (types.getCheckedRadioButtonId()) {
+			      case R.id.sit_down:
+			        r.setType("sit_down");
+			        break;
+			      case R.id.take_out:
+			        r.setType("take_out");
+			        break;
+			      case R.id.delivery:
+			        r.setType("delivery");
+			        break;
+			}
 		}
 	};
 }
