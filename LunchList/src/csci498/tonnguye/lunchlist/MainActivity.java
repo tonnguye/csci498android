0fd758c2dc84143a11b48def81eb0795f8e2be88
Tony Nguyen <tonnguye@mymail.mines.edu>
2012-08-31 18:38:31 -0600

    changed restaurant to an ArrayList

--- LunchList/src/csci498/tonnguye/lunchlist/MainActivity.java
+++ LunchList/src/csci498/tonnguye/lunchlist/MainActivity.java
@@ -6,10 +6,13 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioGroup;
+import java.util.List;
+import java.util.ArrayList;
+
 
 public class MainActivity extends Activity {
 	
-	Restaurant r=new Restaurant();
+	List<Restaurant> model = new ArrayList<Restaurant>();
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -21,24 +24,33 @@ public class MainActivity extends Activity {
 	}
 	private View.OnClickListener onSave=new View.OnClickListener() {
 		public void onClick(View v) {
+			Restaurant r = new Restaurant();
 			EditText name = (EditText)findViewById(R.id.name);
 			EditText address = (EditText)findViewById(R.id.addr);
-
+			
 			r.setName(name.getText().toString());
 			r.setAddress(address.getText().toString());
 			
-			RadioGroup types=(RadioGroup)findViewById(R.id.types);
-			switch (types.getCheckedRadioButtonId()) {
-			      case R.id.sit_down:
-			        r.setType("sit_down");
-			        break;
-			      case R.id.take_out:
-			        r.setType("take_out");
-			        break;
-			      case R.id.delivery:
-			        r.setType("delivery");
-			        break;
-			}
+			addAlotOfRadioButtons(r);
+			
 		}
 	};
+	
+	private void addAlotOfRadioButtons(Restaurant r){
+		
+		RadioGroup types = (RadioGroup)findViewById(R.id.types);
+		switch (types.getCheckedRadioButtonId()) {
+		      case R.id.sit_down:
+		        r.setType("sit_down");
+		        break;
+		      case R.id.take_out:
+		        r.setType("take_out");
+		        break;
+		      case R.id.delivery:
+		        r.setType("delivery");
+		        break;
+		}
+		
+	}
+	
 }
