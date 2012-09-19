c176544ec92afcc14123bfba5a59b36d85e76ae0
Tony Nguyen <tonnguye@mymail.mines.edu>
2012-09-19 10:29:02 -0600

    added sqlite language in order to create a table containing name, text, type, and notes provided by the user

--- LunchList/src/csci498/tonnguye/lunchlist/RestaurantHelper.java
+++ LunchList/src/csci498/tonnguye/lunchlist/RestaurantHelper.java
@@ -3,6 +3,8 @@ package csci498.tonnguye.lunchlist;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.database.Cursor;
+
 
 public class RestaurantHelper extends SQLiteOpenHelper {
 	
@@ -16,11 +18,14 @@ public class RestaurantHelper extends SQLiteOpenHelper {
 	
 	@Override
 	public void onCreate(SQLiteDatabase db) {
+		db.execSQL("CREATE TABLE restaurants (_id INTERGER PRIMARY KEY AUTOINCREMENT, name TEXT, address TEXT, type TEXT, note TEXT);");
 		
 	}
 	
 	@Override
 	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+		//no-op, since will not be called until 2nd schema
+		//version exists...
 		
 	}
 	
