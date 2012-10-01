b8d68e8f314b9efde3412bae06c58d4490e00e6b
Tony Nguyen <tonnguye@mymail.mines.edu>
2012-09-30 22:27:24 -0600

    added inner classed named feedadapter to feedactivity that will help display the results of feed ...

--- LunchList/src/csci498/tonnguye/lunchlist/FeedActivity.java
+++ LunchList/src/csci498/tonnguye/lunchlist/FeedActivity.java
@@ -1,43 +1,50 @@
 package csci498.tonnguye.lunchlist;
 
 import org.mcsoxford.rss.RSSFeed;
+import org.mcsoxford.rss.RSSItem;
 import org.mcsoxford.rss.RSSReader;
 
 import android.app.AlertDialog;
 import android.app.ListActivity;
 import android.os.AsyncTask;
 import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
 
 public class FeedActivity extends ListActivity {
 	private static class FeedTask extends AsyncTask<String, Void, RSSFeed> {
 		private RSSReader reader = new RSSReader();
 		private Exception e = null;
 		private FeedActivity activity = null;
-		
+
 		FeedTask(FeedActivity activity) {
 			attach(activity);
 		}
-		
+
 		void attach(FeedActivity activity) {
-			this.activity=activity;
+			this.activity = activity;
 		}
-		
+
 		void detach() {
-			this.activity=null;
+			this.activity = null;
 		}
-		
+
 		@Override
 		public RSSFeed doInBackground(String... urls) {
 			RSSFeed result=null;
-			
+
 			try {
-				result=reader.load(urls[0]);
+				result = reader.load(urls[0]);
 			}
-			catch (Exception e) {this.e=e;
+			catch (Exception e) {
+				this.e = e;
 			}
 			return(result);
 		}
-		
+
 		@Override
 		public void onPostExecute(RSSFeed feed) {
 			if (e == null) {
@@ -55,4 +62,39 @@ public class FeedActivity extends ListActivity {
 
 		builder.setTitle("Exeception!").setMessage(t.toString()).setPositiveButton("OK", null).show();
 	}
+
+	private class FeedAdapter extends BaseAdapter {
+		RSSFeed feed = null;
+		FeedAdapter(RSSFeed feed) {
+			super();
+			this.feed = feed;
+		}
+		
+		public int getCount() {
+			return(feed.getItems().size());
+		}
+		
+		public Object getItem(int position) {
+			return(feed.getItems().get(position));
+		}
+		
+		public long getItemId(int position) {
+			return(position);
+		}
+		
+		public View getView(int position, View convertView, ViewGroup parent) {
+			View row=convertView;
+			
+			if (row==null) {
+				LayoutInflater inflater=getLayoutInflater();
+				row = inflater.inflate(android.R.layout.simple_list_item_1, parent, false);
+			}
+			
+			RSSItem item=(RSSItem)getItem(position);
+			((TextView)row).setText(item.getTitle());
+			
+			return(row);
+		}
+	}
 }
+
