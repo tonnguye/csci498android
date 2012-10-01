9f045579e7818ce98adc90f9f4a735c60836c55f
Tony Nguyen <tonnguye@mymail.mines.edu>
2012-09-30 22:43:44 -0600

    Added methods to keep feedactivity running when rotating and added FEED_URL. APT 15 End

--- LunchList/src/csci498/tonnguye/lunchlist/FeedActivity.java
+++ LunchList/src/csci498/tonnguye/lunchlist/FeedActivity.java
@@ -7,6 +7,7 @@ import org.mcsoxford.rss.RSSReader;
 import android.app.AlertDialog;
 import android.app.ListActivity;
 import android.os.AsyncTask;
+import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -15,6 +16,9 @@ import android.widget.BaseAdapter;
 import android.widget.TextView;
 
 public class FeedActivity extends ListActivity {
+	static final String FEED_URL = "csci498.tonnguye.FEED_URL";
+	private InstanceState state = null;
+	
 	private static class FeedTask extends AsyncTask<String, Void, RSSFeed> {
 		private RSSReader reader = new RSSReader();
 		private Exception e = null;
@@ -95,6 +99,52 @@ public class FeedActivity extends ListActivity {
 			
 			return(row);
 		}
+	}
+	
+	private static class InstanceState {
+		RSSFeed feed = null;
+		FeedTask task = null;
+		
+	}
+	
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		
+		state = (InstanceState)getLastNonConfigurationInstance();
+		
+		if (state == null) {
+			state = new InstanceState();
+			state.task.execute(getIntent().getStringExtra(FEED_URL));
+			
+		}
+		else {
+			if (state.task != null) {
+				state.task.attach(this);
+				
+			}
+			
+			if (state.feed != null) {
+				setFeed(state.feed);
+				
+			}
+		}
+	}
+	
+	@Override
+	public Object onRetainNonConfigurationInstance() {
+		if (state.task != null) {
+			state.task.detach();
+			
+		}
+		
+		return(state);
+		
+	}
+	
+	private void setFeed(RSSFeed feed) {
+		state.feed = feed;
+		setListAdapter(new FeedAdapter(feed));
 	}
 }
 
