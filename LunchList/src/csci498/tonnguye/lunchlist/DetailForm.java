89b798794cbf3f0f5ec352e3b6000d73d38c531a
Tony Nguyen <tonnguye@mymail.mines.edu>
2012-10-18 17:54:35 -0600

    Created a data member in DetailFormed named locMgr for LocationManager. When user taps location option menu this is what deals with it.

--- LunchList/src/csci498/tonnguye/lunchlist/DetailForm.java
+++ LunchList/src/csci498/tonnguye/lunchlist/DetailForm.java
@@ -4,14 +4,15 @@ package csci498.tonnguye.lunchlist;
 import android.app.Activity;
 import android.content.Intent;
 import android.database.Cursor;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioGroup;
 import android.widget.TextView;
@@ -27,13 +28,15 @@ public class DetailForm extends Activity {
 	RestaurantHelper helper = null;
 	String restaurantId = null;
 	TextView location = null;
+	LocationManager locMgr = null;
 	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.detail_form);
 		
-		helper=new RestaurantHelper(this);
+		helper = new RestaurantHelper(this);
+		locMgr = (LocationManager)getSystemService(LOCATION_SERVICE);
 		
 		location = (TextView)findViewById(R.id.location);
 		name = (EditText)findViewById(R.id.name);
@@ -157,10 +160,37 @@ public class DetailForm extends Activity {
 			else {
 				Toast.makeText(this, "Sorry, the internet is not available", Toast.LENGTH_LONG).show();
 			}
-			return(true);
+			return true;
+		}
+		else if (item.getItemId() == R.id.location) {
+			locMgr.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, onLocationChange);
+			
+			return true;
 		}
 		return(super.onOptionsItemSelected(item));
 	}
+	
+	LocationListener onLocationChange = new LocationListener() {
+		public void onLocationChanged(Location fix) {
+			helper.updateLocation(restaurantId, fix.getLatitude(), fix.getLongitude());
+			location.setText(String.valueOf(fix.getLatitude()) + ", " + String.valueOf(fix.getLongitude()));
+			locMgr.removeUpdates(onLocationChange);
+			
+			Toast.makeText(DetailForm.this, "Location saved", Toast.LENGTH_LONG).show();
+		}
+		
+		public void onProviderDisabled(String provider) {
+			//required for interface, not used
+		}
+		
+		public void onProviderEnabled(String provider) {
+			//required for interface, not used
+		}
+		
+		public void onStatusChanged(String provider, int status, Bundle extras) {
+			//required for interface, not used
+		}
+	};
 	
 	private boolean isNetworkAvailable() {
 		ConnectivityManager cm = (ConnectivityManager)getSystemService(CONNECTIVITY_SERVICE);
