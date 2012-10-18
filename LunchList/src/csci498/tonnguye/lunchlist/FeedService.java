08083307fd88fb8796c36a84eb73feebcba90fbc
Tony Nguyen <tonnguye@mymail.mines.edu>
2012-10-17 22:30:56 -0600

    Created new Java class file named FeedService. It extends IntentService and added this service to androidmanifest.xml

---
+++ LunchList/src/csci498/tonnguye/lunchlist/FeedService.java
@@ -0,0 +1,16 @@
+package csci498.tonnguye.lunchlist;
+
+import android.app.IntentService;
+import android.content.Intent;
+
+public class FeedService extends IntentService {
+	public FeedService() {
+		super("FeedService");
+	}
+	
+	@Override 
+	public void onHandleIntent(Intent i) {
+		//do stuff
+	}
+
+}
