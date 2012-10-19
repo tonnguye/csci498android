4acf01c314d80a3ed746b62cd0310ed6f600bd86
Tony Nguyen <tonnguye@mymail.mines.edu>
2012-10-18 18:55:09 -0600

    Reverted back to the second to last commit, where I ended off in APT 17. Turns out the error that was breaking the application is the SQL database query. If I replace lat, lon inside SELECT_ORDER_BY with feed it works again.

--- LunchList/src/csci498/tonnguye/lunchlist/DetailForm.java
+++ LunchList/src/csci498/tonnguye/lunchlist/DetailForm.java
@@ -4,16 +4,18 @@ package csci498.tonnguye.lunchlist;
 import android.app.Activity;
 import android.content.Intent;
 import android.database.Cursor;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioGroup;
+import android.widget.TextView;
 import android.widget.Toast;
 
 
@@ -25,23 +27,23 @@ public class DetailForm extends Activity {
 	RadioGroup types = null;
 	RestaurantHelper helper = null;
 	String restaurantId = null;
+	TextView location = null;
+	LocationManager locMgr = null;
 	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.detail_form);
 		
-		helper=new RestaurantHelper(this);
+		helper = new RestaurantHelper(this);
+		locMgr = (LocationManager)getSystemService(LOCATION_SERVICE);
 		
+		location = (TextView)findViewById(R.id.location);
 		name = (EditText)findViewById(R.id.name);
 		address = (EditText)findViewById(R.id.addr);
 		notes = (EditText)findViewById(R.id.notes);
 		feed = (EditText)findViewById(R.id.feed);
 		types = (RadioGroup)findViewById(R.id.types);
-		
-		Button save=(Button)findViewById(R.id.save);
-		
-		save.setOnClickListener(onSave);
 		
 		restaurantId = getIntent().getStringExtra(LunchList.ID_EXTRA);
 		
@@ -69,6 +71,8 @@ public class DetailForm extends Activity {
 			types.check(R.id.delivery);
 		}
 		
+		location.setText(String.valueOf(helper.getLatitude(c)) + ", " + String.valueOf(helper.getLongitude(c)));
+		
 		c.close();
 	}
 	
@@ -79,8 +83,8 @@ public class DetailForm extends Activity {
 		helper.close();
 	}
 	
-	private View.OnClickListener onSave=new View.OnClickListener() {
-		public void onClick(View v) {
+	private void save() {
+		if (name.getText().toString().length() > 0) {
 			String type = null;
 			
 			switch (types.getCheckedRadioButtonId()) {
@@ -94,7 +98,7 @@ public class DetailForm extends Activity {
 				type="delivery";
 				break;
 			}
-			if (restaurantId==null) {
+			if (restaurantId == null) {
 				helper.insert(name.getText().toString(),
 						address.getText().toString(), type,
 						notes.getText().toString(),
@@ -106,9 +110,16 @@ public class DetailForm extends Activity {
 						notes.getText().toString(),
 						feed.getText().toString());
 			}
-			finish();
 		}
-	};
+	}
+	
+	@Override
+	public void onPause() {
+		save();
+		locMgr.removeUpdates(onLocationChange);
+		
+		super.onPause();
+	}
 	
 	@Override
 	public void onSaveInstanceState(Bundle state) {
@@ -150,11 +161,45 @@ public class DetailForm extends Activity {
 			else {
 				Toast.makeText(this, "Sorry, the internet is not available", Toast.LENGTH_LONG).show();
 			}
-			return(true);
+			return true;
+		}
+		else if (item.getItemId() == R.id.location) {
+			locMgr.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, onLocationChange);
+			
+			return true;
 		}
 		return(super.onOptionsItemSelected(item));
 	}
 	
+	LocationListener onLocationChange = new LocationListener() {
+		public void onLocationChanged(Location fix) {
+			helper.updateLocation(restaurantId, fix.getLatitude(), fix.getLongitude());
+			location.setText(String.valueOf(fix.getLatitude()) + ", " + String.valueOf(fix.getLongitude()));
+			locMgr.removeUpdates(onLocationChange);
+			
+			Toast.makeText(DetailForm.this, "Location saved", Toast.LENGTH_LONG).show();
+		}
+		
+		public void onProviderDisabled(String provider) {
+			//required for interface, not used
+		}
+		
+		public void onProviderEnabled(String provider) {
+			//required for interface, not used
+		}
+		
+		public void onStatusChanged(String provider, int status, Bundle extras) {
+			//required for interface, not used
+		}
+	};
+	
+	@Override
+	public boolean onPrepareOptionsMenu(Menu menu) {
+		if (restaurantId == null) {
+			menu.findItem(R.id.location).setEnabled(false);
+		}
+		return super.onPrepareOptionsMenu(menu);
+	}
 	private boolean isNetworkAvailable() {
 		ConnectivityManager cm = (ConnectivityManager)getSystemService(CONNECTIVITY_SERVICE);
 		NetworkInfo info = cm.getActiveNetworkInfo();
