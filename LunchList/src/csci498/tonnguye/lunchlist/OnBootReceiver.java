7cc55e7e8aeb6facc207858b6f13b11392741edb
Tony Nguyen <tonnguye@mymail.mines.edu>
2012-10-26 14:28:53 -0600

    Created OnBootReceive class

---
+++ LunchList/src/csci498/tonnguye/lunchlist/OnBootReceiver.java
@@ -0,0 +1,12 @@
+package csci498.tonnguye.lunchlist;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class OnBootReceiver extends BroadcastReceiver {
+	@Override
+	public void onReceive(Context ctxt, Intent intent) {
+		//do stuff
+	}
+}
