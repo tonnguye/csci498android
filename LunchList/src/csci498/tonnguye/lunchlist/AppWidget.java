04ff6e1573fd69ef2250a8b8aa19a442e586e935
Tony Nguyen <tonnguye@mymail.mines.edu>
2012-11-07 17:53:27 -0700

    Added onHCUpdate to AppWidget.java. It Iterates over all app widgets defined in home screen, creates an intent that follows the turles of AdapterView_based app widgets, creates a remote views for attaching the Intent, and creates a pendingintent for identifying that we want clocks on items in the listview and updates the app widget on the screen.

--- LunchList/src/csci498/tonnguye/lunchlist/AppWidget.java
+++ LunchList/src/csci498/tonnguye/lunchlist/AppWidget.java
@@ -1,10 +1,13 @@
 package csci498.tonnguye.lunchlist;
 
+import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Build;
+import android.widget.RemoteViews;
 
 public class AppWidget extends AppWidgetProvider {
 	
@@ -16,6 +19,27 @@ public class AppWidget extends AppWidgetProvider {
 		else {
 			ctxt.startService(new Intent(ctxt, WidgetService.class));
 		}
+	}
+	
+	public void onHCUpdate(Context ctxt, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+		for (int i = 0; i < appWidgetIds.length; i++) {
+			Intent svcIntent = new Intent(ctxt, ListWidgetService.class);
+			
+			svcIntent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, appWidgetIds[i]);
+			svcIntent.setData(Uri.parse(svcIntent.toUri(Intent.URI_INTENT_SCHEME)));
+			
+			RemoteViews widget = new RemoteViews(ctxt.getPackageName(), R.layout.widget);
+			
+			widget.setRemoteAdapter(appWidgetIds[i], R.id.restaurants, svcIntent);
+			
+			Intent clickIntent = new Intent(ctxt, DetailForm.class);
+			PendingIntent clickPI = PendingIntent.getActivity(ctxt, 0, clickIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+			
+			widget.setPendingIntentTemplate(R.id.restaurants, clickPI);
+			
+			appWidgetManager.updateAppWidget(appWidgetIds[i], widget);
+		}
+		super.onUpdate(ctxt, appWidgetManager, appWidgetIds);
 	}
 
 }
