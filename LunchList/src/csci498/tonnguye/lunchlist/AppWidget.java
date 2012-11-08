4cc959978483de8600b17d80493f3565acc0f070
Tony Nguyen <tonnguye@mymail.mines.edu>
2012-11-07 17:35:44 -0700

    Added honeycomb-specific app widget logic to a different code path in onUpdate inside AppWidget.java

--- LunchList/src/csci498/tonnguye/lunchlist/AppWidget.java
+++ LunchList/src/csci498/tonnguye/lunchlist/AppWidget.java
@@ -4,12 +4,18 @@ import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 
 public class AppWidget extends AppWidgetProvider {
 	
 	@Override
 	public void onUpdate(Context ctxt, AppWidgetManager mgr, int[] appWidgetIds) {
-		ctxt.startService(new Intent(ctxt, WidgetService.class));
+		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+			onHCUpdate(ctxt, mgr, appWidgetIds);
+		}
+		else {
+			ctxt.startService(new Intent(ctxt, WidgetService.class));
+		}
 	}
 
 }
