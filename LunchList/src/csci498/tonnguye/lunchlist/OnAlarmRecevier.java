3c729488658a6a78b264c4e1ea926dab8c885a60
Tony Nguyen <tonnguye@mymail.mines.edu>
2012-11-19 16:19:42 -0700

    Edited preferences.xml to have a ringtone Perference. Inside OnAlarmReceiver it check for the preference for alarm and convert the string representation into a Uri and put that in the sound data member of the notification.

--- LunchList/src/csci498/tonnguye/lunchlist/OnAlarmRecevier.java
+++ LunchList/src/csci498/tonnguye/lunchlist/OnAlarmRecevier.java
@@ -8,6 +8,8 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.media.AudioManager;
+import android.net.Uri;
 import android.preference.PreferenceManager;
 
 public class OnAlarmRecevier extends BroadcastReceiver {
@@ -27,6 +29,12 @@ public class OnAlarmRecevier extends BroadcastReceiver {
 			note.setLatestEventInfo(ctxt, "LunchList", "It's time for lunch! Aren't you hungry?", i);
 			note.flags|=Notification.FLAG_AUTO_CANCEL;
 			
+			String sound = prefs.getString("alarm_ringtone", null);
+			
+			if (sound != null) {
+				note.sound = Uri.parse(sound);
+				note.audioStreamType = AudioManager.STREAM_ALARM;
+			}
 			mgr.notify(NOTIFY_ME_ID, note);
 		}
 		else {
